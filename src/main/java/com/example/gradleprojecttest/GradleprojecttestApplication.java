8bdd9c1453a7ffd8cdd67f60aedcdd9dafea1068
Arun Kumar Bandi <abandi@evoketechnologies.com>
2022-09-14 16:29:25 +0530

    initial commit

---
+++ src/main/java/com/example/gradleprojecttest/GradleprojecttestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradleprojecttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleprojecttestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleprojecttestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gradleprojecttest/GradleprojecttestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradleprojecttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleprojecttestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
